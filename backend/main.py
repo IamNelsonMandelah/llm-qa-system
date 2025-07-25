6c298837032090822de8690f371bef47536d76d1
Nelson Omondi <nelson.omondi@pesapal.com>
2025-07-26 01:04:51 +0300

    Added backend, front all together after testing

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 #     "http://localhost:3000",
 #     "http://127.0.0.1:3000",
 #     "http://192.168.139.239:3000",
-# ]
+# not neccessary for now
 # app.add_middleware(
 #     CORSMiddleware,
 #     allow_origins=origins,
